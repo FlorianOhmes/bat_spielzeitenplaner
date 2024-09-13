db3fdff8add565324f9e5bb4dbc1c6d1b6afb2fd
Florian Ohmes <florianohmes@MacBook-Air-von-Florian.local>
2024-09-13 14:01:30 +0200

    (REFC) WelcomePageTest.java: Mvc-Get und JSoup-Select in private Methoden ausgelagert

--- spielzeitenplaner/src/test/java/de/bathesis/spielzeitenplaner/templates/WelcomePageTest.java
+++ spielzeitenplaner/src/test/java/de/bathesis/spielzeitenplaner/templates/WelcomePageTest.java
@@ -6,6 +6,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import java.io.UnsupportedEncodingException;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,12 +25,8 @@ public class WelcomePageTest {
     @DisplayName("Auf der Startseite wird die korrekte Überschrift angezeigt.")
     void test_01() throws Exception {
         String expectedTitle = "Willkommen beim SpielzeitenPlaner!";
-        String html = mvc.perform(get("/"))
-                                  .andReturn()
-                                  .getResponse()
-                                  .getContentAsString();
-        Document welcomePage = Jsoup.parse(html);
-        String pageTitle = welcomePage.select("h1").text();
+        Document welcomePage = getWelcomePage();
+        String pageTitle = extractFrom(welcomePage, "h1");
         assertThat(pageTitle).isEqualTo(expectedTitle);
     }
 
@@ -35,13 +34,24 @@ public class WelcomePageTest {
     @DisplayName("Auf der Startseite wird der Jumbotron korrekt angezeigt.")
     void test_02() throws Exception {
         String expectedJumbotronText = "Der einfachste Weg, deine Jugendfußball-Spiele zu planen und zu verwalten. Nutze unsere Tools, um die Spielzeiten deiner Spieler aufgrund eigens festgelegter Kriterien zu planen.";
-        String html = mvc.perform(get("/"))
-                                  .andReturn()
-                                  .getResponse()
-                                  .getContentAsString();
-        Document welcomePage = Jsoup.parse(html);
-        String jumbotronText = welcomePage.select(".jumbotron p.lead").text();
+        Document welcomePage = getWelcomePage();
+        String jumbotronText = extractFrom(welcomePage, ".jumbotron p.lead");
         assertThat(jumbotronText).isEqualTo(expectedJumbotronText);
+    }
+
+
+    // ausgelagerte Methoden 
+    private Document getWelcomePage() throws Exception {
+        String html = mvc.perform(get("/"))
+                         .andReturn()
+                         .getResponse()
+                         .getContentAsString();
+        Document welcomePage = Jsoup.parse(html);
+        return welcomePage;
+    }
+
+    private String extractFrom(Document welcomePage, String cssQuery) {
+        return welcomePage.select(cssQuery).text();
     }
 
 }
