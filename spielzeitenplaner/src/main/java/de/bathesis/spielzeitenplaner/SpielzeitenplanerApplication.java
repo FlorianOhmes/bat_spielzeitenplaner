cab0bafcfce78557d2c1256f60ee7cc7971d1436
Florian Ohmes <florianohmes@MacBook-Air-von-Florian.local>
2024-09-13 10:15:55 +0200

    Neues Projekt mithilfe von Spring Initializr initialisiert

---
+++ spielzeitenplaner/src/main/java/de/bathesis/spielzeitenplaner/SpielzeitenplanerApplication.java
@@ -0,0 +1,13 @@
+package de.bathesis.spielzeitenplaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpielzeitenplanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpielzeitenplanerApplication.class, args);
+	}
+
+}
---
+++ spielzeitenplaner/src/test/java/de/bathesis/spielzeitenplaner/SpielzeitenplanerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bathesis.spielzeitenplaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpielzeitenplanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
