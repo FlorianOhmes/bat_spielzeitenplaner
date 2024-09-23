64985cfe59a41d5447c3d4af83e5ab13eb607d69
Florian Ohmes <florianohmes@MacBook-Air-von-Florian.local>
2024-09-23 14:24:36 +0200

    (FEAT) Auf der Startseite zur Spielzeitenberechnung wird die Navigationsleiste korrekt angezeigt

--- spielzeitenplaner/src/test/java/de/bathesis/spielzeitenplaner/templates/playingtimes/StartPageTest.java
+++ spielzeitenplaner/src/test/java/de/bathesis/spielzeitenplaner/templates/playingtimes/StartPageTest.java
@@ -8,6 +8,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import de.bathesis.spielzeitenplaner.utilities.ExpectedElements;
 import de.bathesis.spielzeitenplaner.utilities.RequestHelper;
 import de.bathesis.spielzeitenplaner.web.MainController;
 
@@ -33,6 +35,15 @@ class StartPageTest {
         String expectedTitle = "Spielzeiten berechnen";
         String pageTitle = RequestHelper.extractFrom(startPage, "h1");
         assertThat(pageTitle).isEqualTo(expectedTitle);
+    }
+
+    @Test
+    @DisplayName("Auf der Startseite zur Spielzeitenberechnung wird die Navigationsleiste korrekt angezeigt.")
+    void test_02() throws Exception {
+        String navbarBrandText = RequestHelper.extractFrom(startPage, "nav.navbar a.navbar-brand");
+        String navigationItemsTerms = RequestHelper.extractFrom(startPage, "nav.navbar ul.navbar-nav li.nav-item");
+        assertThat(navbarBrandText).isEqualTo(ExpectedElements.NAVBRAND_TEXT);
+        assertThat(navigationItemsTerms).contains(ExpectedElements.FEATURES);
     }
 
 }
