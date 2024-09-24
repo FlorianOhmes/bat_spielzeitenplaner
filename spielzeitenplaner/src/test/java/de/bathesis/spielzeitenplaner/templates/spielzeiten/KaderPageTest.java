bbf06fc0c20eeff4988b8137904fe6780ad39676
Florian Ohmes <florianohmes@MacBook-Air-von-Florian.local>
2024-09-24 11:34:48 +0200

    (FEAT) spielzeiten/kader.html: Es wird die korrekte Überschrift angezeigt(FEAT) spielzeiten/kader.html: Es wird die korrekte Überschrift angezeigt

---
+++ spielzeitenplaner/src/test/java/de/bathesis/spielzeitenplaner/templates/spielzeiten/KaderPageTest.java
@@ -0,0 +1,40 @@
+package de.bathesis.spielzeitenplaner.templates.spielzeiten;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import de.bathesis.spielzeitenplaner.utilities.ExpectedElements;
+import de.bathesis.spielzeitenplaner.utilities.RequestHelper;
+import de.bathesis.spielzeitenplaner.web.SpielzeitenController;
+
+
+@WebMvcTest(SpielzeitenController.class)
+class KaderPageTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    Document kaderPage;
+
+
+    @BeforeEach
+    void getKaderPage() throws Exception {
+        kaderPage = RequestHelper.performGetAndParseWithJSoup(mvc, "/spielzeiten/kader");
+    }
+
+
+    @Test
+    @DisplayName("Auf der Startseite zur Spielzeitenberechnung wird die korrekte Überschrift angezeigt.")
+    void test_01() throws Exception {
+        String expectedTitle = "Spielzeiten berechnen";
+        String pageTitle = RequestHelper.extractFrom(kaderPage, "h1");
+        assertThat(pageTitle).isEqualTo(expectedTitle);
+    }
+
+}
