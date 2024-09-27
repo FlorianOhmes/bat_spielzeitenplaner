1a52ecda280f16c1402ac3f3b0c626997ba27d85
Florian Ohmes <florianohmes@MacBook-Air-von-Florian.local>
2024-09-27 09:16:14 +0200

    (REFC) KaderPageTest.java überarbeitet

--- spielzeitenplaner/src/test/java/de/bathesis/spielzeitenplaner/templates/spielzeiten/KaderPageTest.java
+++ spielzeitenplaner/src/test/java/de/bathesis/spielzeitenplaner/templates/spielzeiten/KaderPageTest.java
@@ -33,14 +33,14 @@ class KaderPageTest {
 
     @Test
     @DisplayName("Auf der Seite Kader der Spielzeitenplanung wird die korrekte Überschrift angezeigt.")
-    void test_01() throws Exception {
+    void test_01() {
         String pageTitle = RequestHelper.extractTextFrom(kaderPage, "h1");
         assertThat(pageTitle).isEqualTo(ExpectedElements.spielzeitenTitle());
     }
 
     @Test
     @DisplayName("Auf der Seite Kader der Spielzeitenplanung wird die Navigationsleiste korrekt angezeigt.")
-    void test_02() throws Exception {
+    void test_02() {
         String navbarBrandText = RequestHelper.extractTextFrom(kaderPage, "nav.navbar a.navbar-brand");
         String navigationItemsTerms = RequestHelper.extractTextFrom(kaderPage, "nav.navbar ul.navbar-nav li.nav-item");
         assertThat(navbarBrandText).isEqualTo(ExpectedElements.navbrandText());
@@ -49,29 +49,29 @@ class KaderPageTest {
 
     @Test
     @DisplayName("Auf der Seite Kader der Spielzeitenplanung wird der Footer korrekt angezeigt.")
-    void test_03() throws Exception {
+    void test_03() {
         String footerText = RequestHelper.extractTextFrom(kaderPage, "footer p");
         assertThat(footerText).isEqualTo(ExpectedElements.footerText());
     }
 
     @Test
     @DisplayName("Auf der Seite Kader der Spielzeitenplanung wird ein Paragraph mit einer kurzen Erklärung angezeigt.")
-    void test_04() throws Exception {
+    void test_04() {
         String leadText = RequestHelper.extractTextFrom(kaderPage, "p.lead");
         assertThat(leadText).isNotBlank();
     }
 
     @Test
     @DisplayName("Auf der Seite Kader der Spielzeitenplanung wird der Bereich Kader bestätigen korrekt angezeigt.")
-    void test_05() throws Exception {
+    void test_05() {
         List<String> expectedH2Titles = new ArrayList<>(List.of(
             "Im Kader", "Nicht dabei"
         ));
 
-        Elements form = kaderPage.select("form#kader");
-        String h2Titles = RequestHelper.extractTextFrom(kaderPage, "form#kader h2");
-        Elements playersIn = form.select("#playersIn");
-        Elements playersOut = form.select("#playersOut");
+        Elements form = RequestHelper.extractFrom(kaderPage, "form#kader");
+        String h2Titles = RequestHelper.extractTextFrom(form, "h2");
+        Elements playersIn = RequestHelper.extractFrom(form, "#playersIn");
+        Elements playersOut = RequestHelper.extractFrom(form, "#playersOut");
 
         assertThat(form).isNotEmpty();
         assertThat(h2Titles).contains(expectedH2Titles);
