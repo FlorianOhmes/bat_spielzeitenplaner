dd90bcacf00a963bd127d0fc0808fa0fe5c5a0cd
Florian Ohmes <florianohmes@MacBook-Air-von-Florian.local>
2024-10-06 14:25:08 +0200

    (REFC) Einführung des TeamRepositories als Interface und seiner Implementierung TeamRepositoryImpl

---
+++ spielzeitenplaner/src/main/java/de/bathesis/spielzeitenplaner/database/TeamRepositoryImpl.java
@@ -0,0 +1,18 @@
+package de.bathesis.spielzeitenplaner.database;
+
+import org.springframework.stereotype.Repository;
+import de.bathesis.spielzeitenplaner.domain.Team;
+import de.bathesis.spielzeitenplaner.services.TeamRepository;
+
+
+@Repository
+public class TeamRepositoryImpl implements TeamRepository {
+
+    @Override
+    public void save(Team team) {
+        // Implementierung folgt... 
+    }
+
+
+
+}
--- spielzeitenplaner/src/main/java/de/bathesis/spielzeitenplaner/services/TeamRepository.java
+++ spielzeitenplaner/src/main/java/de/bathesis/spielzeitenplaner/services/TeamRepository.java
@@ -1,14 +1,10 @@
 package de.bathesis.spielzeitenplaner.services;
 
-import org.springframework.stereotype.Repository;
 import de.bathesis.spielzeitenplaner.domain.Team;
 
 
-@Repository
-public class TeamRepository {
+public interface TeamRepository {
 
-    public void save(Team team) {
-        // Implementierung folgt... 
-    }
+    void save(Team team);
 
 }
