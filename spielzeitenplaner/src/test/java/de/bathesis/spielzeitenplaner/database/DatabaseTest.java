544fbd59c6e7b29b8ff0637318dd99c6da1f7344
Florian Ohmes <florianohmes@MacBook-Air-von-Florian.local>
2024-10-09 14:41:23 +0200

    (REFC) DatabaseTest.java überarbeitet

--- spielzeitenplaner/src/test/java/de/bathesis/spielzeitenplaner/database/DatabaseTest.java
+++ spielzeitenplaner/src/test/java/de/bathesis/spielzeitenplaner/database/DatabaseTest.java
@@ -8,6 +8,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -16,8 +19,8 @@ import de.bathesis.spielzeitenplaner.services.TeamRepository;
 
 
 @DataJdbcTest
-@Testcontainers
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
 public class DatabaseTest {
 
     @Container
@@ -50,20 +53,18 @@ public class DatabaseTest {
     }
 
     @Test
-    @DisplayName("Wenn bereits ein Eintrag für den Teamnamen vorhanden ist, wird der Teamname geupdated.")
+    @DisplayName("Es werden alle Einträge in der Team-Tabelle gefunden.")
     public void test_02() {
-        Team team = new Team(null, "Spring Boot FC");
-        Team saved = repository.save(team);
-        Team newTeam = new Team(saved.id(), "FC Heidenheim");
+        Team schalke = new Team(null, "FC Schalke 04");
+        Team dortmund = new Team(null, "Borussia Dortmund");
+        Team bayern = new Team(null, "FC Bayern München");
+        Team savedSchalke = repository.save(schalke);
+        Team savedDortmund = repository.save(dortmund);
+        Team savedBayern = repository.save(bayern);
 
-        Team updated = repository.save(newTeam);
-
-        Optional<Team> loaded = repository.findById(updated.id());
         Collection<Team> allEntries = repository.findAll();
 
-        assertThat(loaded.map(Team::id).orElseThrow()).isEqualTo(saved.id());
-        assertThat(loaded.map(Team::name).orElseThrow()).isEqualTo(newTeam.name());
-        assertThat(allEntries.size()).isOne();
+        assertThat(allEntries).contains(savedSchalke, savedDortmund, savedBayern);
     }
 
 }
