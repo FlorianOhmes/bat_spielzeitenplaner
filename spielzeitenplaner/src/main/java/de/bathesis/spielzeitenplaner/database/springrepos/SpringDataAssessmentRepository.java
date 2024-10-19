f3a34018f1a2bfdf3833c6022cb91a5ae08f34ab
Florian Ohmes <florianohmes@MacBook-Air-von-Florian.local>
2024-10-19 17:22:04 +0200

    (FEAT) SpringDataAssessmentRepository.java überarbeitet

--- spielzeitenplaner/src/main/java/de/bathesis/spielzeitenplaner/database/springrepos/SpringDataAssessmentRepository.java
+++ spielzeitenplaner/src/main/java/de/bathesis/spielzeitenplaner/database/springrepos/SpringDataAssessmentRepository.java
@@ -2,10 +2,12 @@ package de.bathesis.spielzeitenplaner.database.springrepos;
 
 import org.springframework.data.repository.CrudRepository;
 import de.bathesis.spielzeitenplaner.database.entities.Assessment;
+import java.util.Optional;
 
 
 public interface SpringDataAssessmentRepository extends CrudRepository<Assessment, Integer> {
 
-
+    Assessment save(Assessment assessment);
+    Optional<Assessment> findById(Integer id);
 
 }
