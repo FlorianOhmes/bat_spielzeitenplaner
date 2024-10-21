be1e06c81ea469093f99a800b178ac7595fc4216
Florian Ohmes <florianohmes@MacBook-Air-von-Florian.local>
2024-10-21 14:57:10 +0200

    (FEAT) PlayerService: Die calculateScore-Methode funktioniert auch für das spezielle Kriterium Trainingsbeteiligung

--- spielzeitenplaner/src/main/java/de/bathesis/spielzeitenplaner/services/PlayerService.java
+++ spielzeitenplaner/src/main/java/de/bathesis/spielzeitenplaner/services/PlayerService.java
@@ -5,10 +5,14 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import de.bathesis.spielzeitenplaner.domain.Assessment;
+import de.bathesis.spielzeitenplaner.domain.Criterion;
 import de.bathesis.spielzeitenplaner.domain.Player;
 import de.bathesis.spielzeitenplaner.services.repos.AssessmentRepository;
+import de.bathesis.spielzeitenplaner.services.repos.CriterionRepository;
 import de.bathesis.spielzeitenplaner.services.repos.PlayerRepository;
 import de.bathesis.spielzeitenplaner.services.repos.SettingRepository;
 
@@ -18,14 +22,16 @@ public class PlayerService {
 
     private final PlayerRepository playerRepository;
 
+    private final CriterionRepository criterionRepository;
     private final AssessmentRepository assessmentRepository;
     private final SettingRepository settingRepository;
 
     public PlayerService(PlayerRepository playerRepository, AssessmentRepository assessmentRepository, 
-                         SettingRepository settingRepository) {
+                         SettingRepository settingRepository, CriterionRepository criterionRepository) {
         this.playerRepository = playerRepository;
         this.assessmentRepository = assessmentRepository;
         this.settingRepository = settingRepository;
+        this.criterionRepository = criterionRepository;
     }
 
     public void deletePlayer(Integer id) {
@@ -59,6 +65,45 @@ public class PlayerService {
     }
 
     public Double calculateScore(Integer criterionId, Integer playerId) {
+        Optional<Criterion> criterion = criterionRepository.findById(criterionId);
+        if (!criterion.isPresent()) {
+            return 0.0;
+        }
+
+        if (criterion.get().getName().equals("Trainingsbeteiligung")) {
+            return calculateScoreTraining(criterionId, playerId);
+        }
+
+        return calculateScoreOther(criterionId, playerId);
+    }
+
+    private Double calculateScoreTraining(Integer criterionId, Integer playerId) {
+        Integer weeksShortTerm = settingRepository.findById(1196).get().getValue().intValue();
+        Double weightShortTerm = settingRepository.findById(1197).get().getValue();
+        Integer weeksLongTerm = settingRepository.findById(1198).get().getValue().intValue();
+        Double weightLongTerm = settingRepository.findById(1199).get().getValue();
+
+        LocalDate dateShortTerm = LocalDate.now().minusWeeks(weeksShortTerm).minusDays(1);
+        LocalDate dateLongTerm = LocalDate.now().minusWeeks(weeksLongTerm).minusDays(1);
+
+        Collection<Assessment> assessmentsShortTerm = 
+            assessmentRepository.findByPlayerIdAndCriterionIdAndDateAfter(playerId, criterionId, dateShortTerm);
+        Collection<Assessment> assessmentsLongTerm = 
+            assessmentRepository.findByPlayerIdAndCriterionIdAndDateAfter(playerId, criterionId, dateLongTerm);
+
+        Long countShortTerm = assessmentsShortTerm.stream().filter(a -> a.getValue() > 0.0).count();
+        Long totalShortTerm = assessmentsShortTerm.stream().count();
+        Double scoreShortTerm = (double) (countShortTerm) / (double) (totalShortTerm);
+
+        Long countLongTerm = assessmentsLongTerm.stream().filter(a -> a.getValue() > 0.0).count();
+        Long totalLongTerm = assessmentsLongTerm.stream().count();
+        Double scoreLongTerm = (double) (countLongTerm) / (double) (totalLongTerm);
+
+        Double score = weightShortTerm * scoreShortTerm + weightLongTerm * scoreLongTerm;
+        return score;
+    }
+
+    private Double calculateScoreOther(Integer criterionId, Integer playerId) {
         Integer weeksToSubtract = settingRepository.findById(1195).get().getValue().intValue();
         LocalDate date = LocalDate.now().minusWeeks(weeksToSubtract).minusDays(1);
 
--- spielzeitenplaner/src/test/java/de/bathesis/spielzeitenplaner/services/PlayerServiceTest.java
+++ spielzeitenplaner/src/test/java/de/bathesis/spielzeitenplaner/services/PlayerServiceTest.java
@@ -11,9 +11,11 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import de.bathesis.spielzeitenplaner.domain.Assessment;
+import de.bathesis.spielzeitenplaner.domain.Criterion;
 import de.bathesis.spielzeitenplaner.domain.Player;
 import de.bathesis.spielzeitenplaner.domain.Setting;
 import de.bathesis.spielzeitenplaner.services.repos.AssessmentRepository;
+import de.bathesis.spielzeitenplaner.services.repos.CriterionRepository;
 import de.bathesis.spielzeitenplaner.services.repos.PlayerRepository;
 import de.bathesis.spielzeitenplaner.services.repos.SettingRepository;
 import de.bathesis.spielzeitenplaner.utilities.TestObjectGenerator;
@@ -26,8 +28,9 @@ class PlayerServiceTest {
 
     AssessmentRepository assessmentRepository = mock(AssessmentRepository.class);
     SettingRepository settingRepository = mock(SettingRepository.class);
+    CriterionRepository criterionRepository = mock(CriterionRepository.class);
 
-    PlayerService playerService = new PlayerService(playerRepository, assessmentRepository, settingRepository);
+    PlayerService playerService = new PlayerService(playerRepository, assessmentRepository, settingRepository, criterionRepository);
 
     @Test
     @DisplayName("Ein Spieler wird gelöscht.")
@@ -90,21 +93,60 @@ class PlayerServiceTest {
     @DisplayName("Die calculateScore-Methode funktioniert korrekt.")
     void test_06() {
         Integer playerId = 1;
-        Integer criterionId = 1;
+        Criterion criterion = new Criterion(2, "Sozialverhalten", "S", 0.25);
         Setting weeksGeneral = new Setting(1195, "weeksGeneral", 4.0);
         List<Assessment> assessments = new ArrayList<>(List.of(
-            new Assessment(1, LocalDate.now(), criterionId, playerId, 2.0),
-            new Assessment(2, LocalDate.now(), criterionId, playerId, 4.0),
-            new Assessment(3, LocalDate.now(), criterionId, playerId, 3.0)
+            new Assessment(1, LocalDate.now(), criterion.getId(), playerId, 2.0),
+            new Assessment(2, LocalDate.now(), criterion.getId(), playerId, 4.0),
+            new Assessment(3, LocalDate.now(), criterion.getId(), playerId, 3.0)
         ));
         Double expectedScore = assessments.stream().mapToDouble(Assessment::getValue).average().orElseThrow();
 
+        when(criterionRepository.findById(criterion.getId())).thenReturn(Optional.of(criterion));
         when(settingRepository.findById(weeksGeneral.getId())).thenReturn(Optional.of(weeksGeneral));
         when(assessmentRepository.findByPlayerIdAndCriterionIdAndDateAfter(
-            playerId, criterionId, LocalDate.now().minusWeeks(weeksGeneral.getValue().intValue()).minusDays(1))
+            playerId, criterion.getId(), LocalDate.now().minusWeeks(weeksGeneral.getValue().intValue()).minusDays(1))
         ).thenReturn(assessments);
 
-        Double score = playerService.calculateScore(criterionId, playerId);
+        Double score = playerService.calculateScore(criterion.getId(), playerId);
+
+        assertThat(score).isEqualTo(expectedScore);
+    }
+
+    @Test
+    @DisplayName("Die calculateScore-Methode funktioniert auch für das spezielle Kriterium Trainingsbeteiligung korrekt.")
+    void test_07() {
+        Integer playerId = 1;
+        Criterion criterion = new Criterion(1, "Trainingsbeteiligung", "T", 0.35);
+        Setting weeksShortTerm = new Setting(1196, "weeksShortTerm", 2.0);
+        Setting weightShortTerm = new Setting(1197, "weightShortTerm", 0.5);
+        Setting weeksLongTerm = new Setting(1198, "weeksLongTerm", 12.0);
+        Setting weightLongTerm = new Setting(1199, "weightLongTerm", 0.5);
+        List<Assessment> assessmentsShortTerm = new ArrayList<>(List.of(
+            new Assessment(1, LocalDate.now(), criterion.getId(), playerId, 0.0),
+            new Assessment(2, LocalDate.now(), criterion.getId(), playerId, 1.0),
+            new Assessment(3, LocalDate.now(), criterion.getId(), playerId, 0.0)
+        ));
+        List<Assessment> assessmentsLongTerm = new ArrayList<>(assessmentsShortTerm);
+        assessmentsLongTerm.add(new Assessment(4, LocalDate.now(), criterion.getId(), playerId, 1.0));
+        assessmentsLongTerm.add(new Assessment(5, LocalDate.now(), criterion.getId(), playerId, 1.0));
+        assessmentsLongTerm.add(new Assessment(6, LocalDate.now(), criterion.getId(), playerId, 1.0));
+        Double expectedScore = weightShortTerm.getValue() * (1.0 / 3.0) + weightLongTerm.getValue() * (4.0 / 6.0);
+
+        when(criterionRepository.findById(criterion.getId())).thenReturn(Optional.of(criterion));
+        when(settingRepository.findById(weeksShortTerm.getId())).thenReturn(Optional.of(weeksShortTerm));
+        when(settingRepository.findById(weightShortTerm.getId())).thenReturn(Optional.of(weightShortTerm));
+        when(settingRepository.findById(weeksLongTerm.getId())).thenReturn(Optional.of(weeksLongTerm));
+        when(settingRepository.findById(weightLongTerm.getId())).thenReturn(Optional.of(weightLongTerm));
+        when(assessmentRepository.findByPlayerIdAndCriterionIdAndDateAfter(
+            playerId, criterion.getId(), LocalDate.now().minusWeeks(weeksShortTerm.getValue().intValue()).minusDays(1)
+        )).thenReturn(assessmentsShortTerm);
+        when(assessmentRepository.findByPlayerIdAndCriterionIdAndDateAfter(
+            playerId, criterion.getId(), LocalDate.now().minusWeeks(weeksLongTerm.getValue().intValue()).minusDays(1)
+        )).thenReturn(assessmentsLongTerm);
+
+
+        Double score = playerService.calculateScore(criterion.getId(), playerId);
 
         assertThat(score).isEqualTo(expectedScore);
     }
